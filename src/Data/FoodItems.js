de371aa46ec97f594789848aed66324632f7fb0e
alinauman97 <alinauman97@outlook.com>
2020-05-07 03:07:43 +0500

    Added additional food items

--- src/Data/FoodItems.js
+++ src/Data/FoodItems.js
@@ -31,6 +31,46 @@ let foodItems = [
         serving: 2,
         availableOnDay: 3
     },
+    {
+        id: 5,
+        name: "Daal (Moong)",
+        image: "Assets/Images/5.jpg",
+        price: 500,
+        serving: 2,
+        availableOnDay: 4
+    },
+    {
+        id: 6,
+        name: "Nihari (Chicken)",
+        image: "Assets/Images/6.jpg",
+        price: 500,
+        serving: 2,
+        availableOnDay: 5
+    },
+    {
+        id: 7,
+        name: "Spicy Tawa Qeema (Mutton)",
+        image: "Assets/Images/7.jpg",
+        price: 500,
+        serving: 2,
+        availableOnDay: 6
+    },
+    {
+        id: 8,
+        name: "Hyderabadi Haleem",
+        image: "Assets/Images/8.jpg",
+        price: 500,
+        serving: 2,
+        availableOnDay: 7
+    },
+    {
+        id: 9,
+        name: "Fruit Chaat",
+        image: "Assets/Images/9.jpg",
+        price: 500,
+        serving: 2,
+        availableOnDay: 0
+    },
 ];
 
 export default foodItems;
