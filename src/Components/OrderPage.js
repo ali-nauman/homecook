dc874b4f9f5804e624ba0ec9b9f3d56285e5a61c
alinauman97 <alinauman97@outlook.com>
2020-06-07 01:45:56 +0500

    Tweaked the appearence

--- src/Components/OrderPage.js
+++ src/Components/OrderPage.js
@@ -13,7 +13,7 @@ function getTotalCost(items) {
 }
 
 const OrderPage = (props) => {
-    const title = <h3 className="mt-4">Order</h3>
+    const title = <h3>Order</h3>
     const message = <p>Here you can view the details of your order</p>
 
     if (props.orderItems.length === 0) {
@@ -31,40 +31,38 @@ const OrderPage = (props) => {
             <td>{item.item.name}</td>
             <td>{item.item.serving}</td>
             <td>Rs. {item.item.price}</td>
-            <td>{item.quantity}</td>
-            <td>{item.item.serving * item.quantity}</td>
+            <td>{item.quantity} (serves {item.item.serving * item.quantity})</td>
             <td>Rs. {item.quantity * item.item.price}</td>
         </tr>
     );
 
     return (
-        <>
-            {title}
-            {message}
+            <div className="mx-4 mt-4">
+                {title}
+                {message}
 
-            <Table striped bordered hover>
-                <thead>
+                <Table bordered hover>
+                    <thead className="thead-dark">
+                        <tr>
+                            <th>#</th>
+                            <th>Name</th>
+                            <th>Serving</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Subtotal</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {orderItems}
+                    </tbody>
+
                     <tr>
-                        <th>#</th>
-                        <th>Name</th>
-                        <th>Serving</th>
-                        <th>Price</th>
-                        <th>Quantity</th>
-                        <th>Total Serving</th>
-                        <th>Subtotal</th>
+                        <td className="text-right font-weight-bold" colspan="5">Total</td>
+                        <td>{getTotalCost(props.orderItems)}</td>
                     </tr>
-                </thead>
-
-                <tbody>
-                    {orderItems}
-                </tbody>
-
-                <tr>
-                    <td className="text-right font-weight-bold" colspan="6">Total</td>
-                    <td>{getTotalCost(props.orderItems)}</td>
-                </tr>
-            </Table>
-        </>
+                </Table>
+            </div>
     )
 }
 
