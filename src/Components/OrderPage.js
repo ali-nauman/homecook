3d4db63007912e2ab013cd89a620b86c8cf3d250
alinauman97 <alinauman97@outlook.com>
2020-06-14 13:29:17 +0500

    Added missing semicolons, added button to return to homepage, added key to each orderItem

--- src/Components/OrderPage.js
+++ src/Components/OrderPage.js
@@ -4,30 +4,30 @@ import Table from 'react-bootstrap/Table';
 import { Link } from 'react-router-dom';
 
 function getTotalCost(items) {
-    let total = 0
+    let total = 0;
 
     items.forEach(element => {
-        total += element.item.price * element.quantity
+        total += element.item.price * element.quantity;
     });
 
     return total;
 }
 
 const OrderPage = (props) => {
-    const title = <h3>Order</h3>
-    const message = <p>Here you can view the details of your order</p>
+    const title = <h3>Order</h3>;
+    const message = <p>Here you can view the details of your order</p>;
 
     if (props.orderItems.length === 0) {
         return (
             <div className="mx-4 mt-4">
                 {title}
-                <p>You have not added any items to your order yet! Go back to the homepage to add some items!</p>
+                <p>You have not added any items to your order yet! Go back to the <Link to="/">home page</Link> to add some items!</p>
             </div>
-        )
-    };
+        );
+    }
 
     const orderItems = props.orderItems.map((item, i = 0) =>
-        <tr>
+        <tr key={i}>
             <td>{++i}</td>
             <td>{item.item.name}</td>
             <td>{item.item.serving}</td>
@@ -38,35 +38,40 @@ const OrderPage = (props) => {
     );
 
     return (
-            <div className="mx-4 mt-4">
-                {title}
-                {message}
+        <div className="mx-4 mt-4">
+            {title}
+            {message}
 
-                <Table bordered hover>
-                    <thead className="thead-dark">
-                        <tr>
-                            <th>#</th>
-                            <th>Name</th>
-                            <th>Serving</th>
-                            <th>Price</th>
-                            <th>Quantity</th>
-                            <th>Subtotal</th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {orderItems}
-                    </tbody>
-
+            <Table bordered hover>
+                <thead className="thead-dark">
                     <tr>
-                        <td className="text-right font-weight-bold" colspan="5">Total</td>
-                        <td>{getTotalCost(props.orderItems)}</td>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Serving</th>
+                        <th>Price</th>
+                        <th>Quantity</th>
+                        <th>Subtotal</th>
                     </tr>
-                </Table>
+                </thead>
 
+                <tbody>
+                    {orderItems}
+                </tbody>
+
+                <tfoot>
+                    <tr>
+                        <td className="text-right font-weight-bold" colSpan="5">Total</td>
+                        <td>Rs. {getTotalCost(props.orderItems)}</td>
+                    </tr>
+                </tfoot>
+            </Table>
+
+            <div>
                 <Link className="btn btn-primary" to="/checkout">Checkout</Link>
+                <Link className="btn btn-outline-primary ml-4" to="/">Add More Items</Link>
             </div>
-    )
+        </div>
+    );
 }
 
 export default OrderPage;
