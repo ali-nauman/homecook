192879ce58516096e47fb5c5a0a6b8a58e6b17ae
alinauman97 <alinauman97@outlook.com>
2020-06-14 13:26:12 +0500

    Added a message regarding shipping time policy

--- src/Components/CheckoutPage.js
+++ src/Components/CheckoutPage.js
@@ -6,9 +6,10 @@ const CheckoutPage = (props) => {
     return (
         <div className="mt-4 mx-4">
             <h3>Checkout</h3>
-            <p>Enter the details of where<sup>*</sup> you'd like your food delivered</p>
+            <p>Enter the details of where<sup>*</sup> you'd like your food delivered<sup>**</sup></p>
 
-            <small className="text-danger">* Note: We only deliver food within Islamabad!</small>
+            <small className="text-danger">* Note: We only deliver food within Islamabad</small><br></br>
+            <small className="text-danger">** Note: Your order will be delivered after 5 hours as per our policy</small>
             <CheckoutForm></CheckoutForm>
         </div>
     );
