5bfd0490acaa44beeacdd0f76b5f05d9e3e1aa5f
Ali Nauman <alinauman97@outlook.com>
2022-12-11 00:14:11 +0500

    Use createRoot API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import HomeCook from './Components/HomeCook';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { createRoot } from "react-dom/client";
+import HomeCook from "./Components/HomeCook";
 
-ReactDOM.render(
-  <HomeCook></HomeCook>,
-  document.getElementById('root')
-);
+import "bootstrap/dist/css/bootstrap.min.css";
 
-serviceWorker.unregister();
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(<HomeCook />);
