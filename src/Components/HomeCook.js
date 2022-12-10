7dad60bb4fb10b80e0e4eaba3b8272bf9fca9c60
Ali Nauman <alinauman97@outlook.com>
2022-12-11 00:14:23 +0500

    Fix issues with router, list rendering

--- src/Components/HomeCook.js
+++ src/Components/HomeCook.js
@@ -1,75 +1,84 @@
-import React from 'react';
+import React from "react";
 
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav'
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import HomePage from './HomePage';
-import OrderPage from './OrderPage';
-import CheckoutPage from './CheckoutPage';
+import HomePage from "./HomePage";
+import OrderPage from "./OrderPage";
+import CheckoutPage from "./CheckoutPage";
 
-import FoodItems from '../Data/FoodItems';
+import FoodItems from "../Data/FoodItems";
 
 class HomeCook extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            order: [],
-            menu: this.getMenu()
-        };
+    this.state = {
+      order: [],
+      menu: this.getMenu(),
+    };
+  }
+
+  addToOrder(item, servingSize) {
+    let order = this.state.order;
+    let desiredItem = order.find((orderItem) => orderItem.id === item.id);
+
+    if (desiredItem) {
+      desiredItem.quantity = servingSize / item.serving;
+    } else {
+      item.quantity = servingSize / item.serving;
+      order.push(item);
     }
 
-    addToOrder(item, servingSize) {
-        let order = this.state.order;
-        let desiredItem = order.find(orderItem => orderItem.id === item.id);
+    this.setState({
+      order: order,
+    });
+  }
 
-        if (desiredItem) {
-            desiredItem.quantity = servingSize / item.serving;
-        } else {
-            item.quantity = servingSize / item.serving;
-            order.push(item);
-        }
+  getMenu() {
+    return FoodItems.filter(
+      (item) => item.availableOnDay === new Date().getDay()
+    );
+  }
 
-        this.setState({
-            order: order
-        });
-    }
+  render() {
+    return (
+      <BrowserRouter>
+        <Navbar bg="dark" expand="lg" className="navbar-dark">
+          <Navbar.Brand className="text-white mt-1">HomeCook</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link className="text-light mt-2 mx-2" to="/">
+                Home
+              </Link>
+              <Link className="text-light mt-2 mx-2" to="/order">
+                Order
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-    getMenu() {
-        return FoodItems.filter(item => item.availableOnDay === new Date().getDay())
-    }
-
-    render() {
-        return (
-            <Router>
-                <Navbar bg="dark" expand="lg" className="navbar-dark">
-                    <Navbar.Brand className="text-white mt-1">HomeCook</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Link className="text-light mt-2 mx-2" to="/">Home</Link>
-                            <Link className="text-light mt-2 mx-2" to="/order">Order</Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-
-                <Switch>
-                    <Route path="/checkout">
-                        <CheckoutPage></CheckoutPage>
-                    </Route>
-
-                    <Route path="/order">
-                        <OrderPage orderItems={this.state.order}></OrderPage>
-                    </Route>
-
-                    <Route path="/">
-                        <HomePage menu={this.state.menu} onClick={(item, quantity) => this.addToOrder(item, quantity)}></HomePage>
-                    </Route>
-                </Switch>
-            </Router>
-        );
-    }
+        <Routes>
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route
+            path="/order"
+            element={<OrderPage orderItems={this.state.order}></OrderPage>}
+          />
+          <Route
+            path="/"
+            element={
+              <HomePage
+                menu={this.state.menu}
+                onClick={(item, quantity) => this.addToOrder(item, quantity)}
+              ></HomePage>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default HomeCook;
--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,37 +1,37 @@
-import React from 'react';
+import React from "react";
 
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
-import FoodItem from './FoodItem';
+import FoodItem from "./FoodItem";
 
 const HomePage = (props) => {
-    const menu = props.menu.map(item =>
-        <Col xs={12} sm={6} md={6} lg={4} xl={3}>
-            <FoodItem
-                key={item.id}
-                id={item.id}
-                name={item.name}
-                image={item.image}
-                price={item.price}
-                serving={item.serving}
-                availableOnDay={item.availableOnDay}
-                onClick={props.onClick}>
-            </FoodItem>
-        </Col>
-    );
+  const menu = props.menu.map((item) => (
+    <Col key={item.id} xs={12} sm={6} md={6} lg={4} xl={3}>
+      <FoodItem
+        key={item.id}
+        id={item.id}
+        name={item.name}
+        image={item.image}
+        price={item.price}
+        serving={item.serving}
+        availableOnDay={item.availableOnDay}
+        onClick={props.onClick}
+      ></FoodItem>
+    </Col>
+  ));
 
+  return (
+    <div className="mx-4">
+      <h3 className="mt-4 text-white">HomeCook</h3>
+      <p className="text-light">
+        Take a look at the variety of fresh, home-made food that we can deliver
+        at your doorstep!
+      </p>
 
-    return (
-        <div className="mx-4">
-            <h3 className="mt-4 text-white">HomeCook</h3>
-            <p className="text-light">Take a look at the variety of fresh, home-made food that we can deliver at your doorstep!</p>
-
-            <Row className="d-flex justify-content-between">
-                {menu}
-            </Row>
-        </div>
-    );
-}
+      <Row className="d-flex justify-content-between">{menu}</Row>
+    </div>
+  );
+};
 
 export default HomePage;
