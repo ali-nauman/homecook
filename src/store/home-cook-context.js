2ef9a8d898958391c4a4ffcc8b0b68d86a3fed4a
Ali Nauman <alinauman97@outlook.com>
2022-12-11 22:08:02 +0500

    Add HomeCookContext

---
+++ src/store/home-cook-context.js
@@ -0,0 +1,31 @@
+import React, { createContext, useState } from 'react';
+
+import { foodItems } from '../Data/FoodItems';
+
+const getMenu = () => {
+  return foodItems.filter(
+    (item) => item.availableOnDay === new Date().getDay()
+  );
+};
+
+export const HomeCookContext = createContext({
+  order: [],
+  setOrder: () => {},
+  menu: [],
+});
+
+export const HomeCookProvider = ({ children }) => {
+  const [menu] = useState(getMenu());
+  const [order, setOrder] = useState([]);
+
+  const updateOrder = (newOrder) => {
+    console.log('Updating order to', newOrder);
+    setOrder(newOrder);
+  };
+
+  return (
+    <HomeCookContext.Provider value={{ order, setOrder: updateOrder, menu }}>
+      {children}
+    </HomeCookContext.Provider>
+  );
+};
