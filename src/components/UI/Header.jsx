2e8a5963ecbd11e9edff6c93abc63d2e591efe62
Ali Nauman <alinauman97@outlook.com>
2022-12-11 22:49:59 +0500

    Improve styling

--- src/components/UI/Header.jsx
+++ src/components/UI/Header.jsx
@@ -4,27 +4,44 @@ import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 
 import { HomeCookContext } from 'store/home-cook-context';
+import { useLocation } from 'react-router-dom';
+import { NavItem } from 'react-bootstrap';
 
 export const Header = () => {
+  const location = useLocation();
   const { order } = useContext(HomeCookContext);
+
+  const links = [
+    { label: 'Home', path: '/' },
+    {
+      label: 'Order',
+      path: '/order',
+      content: !!order.length && (
+        <span className="badge rounded-pill bg-danger">{order.length}</span>
+      ),
+    },
+  ];
 
   return (
     <Navbar bg="dark" expand="lg" className="navbar-dark">
-      <Navbar.Brand className="text-white mt-1">HomeCook</Navbar.Brand>
+      <Navbar.Brand className="text-white">HomeCook</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Link className="text-light mt-2 mx-2" to="/">
-            Home
-          </Link>
-          <Link className="text-light mt-2 mx-2" to="/order">
-            Order{' '}
-            {!!order.length && (
-              <span className="badge rounded-pill bg-danger">
-                {order.length}
-              </span>
-            )}
-          </Link>
+        <Nav className="d-flex gap-3">
+          {links.map((link) => (
+            <NavItem key={link.path}>
+              <Link
+                className="text-decoration-none"
+                style={{
+                  color:
+                    location.pathname === link.path ? 'dodgerblue' : 'white',
+                }}
+                to={link.path}
+              >
+                {link.label} {link.content}
+              </Link>
+            </NavItem>
+          ))}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
