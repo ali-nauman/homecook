6761e98cdeeb30d9ea46f42e785cc0ae31f2f803
Ali Nauman <alinauman97@outlook.com>
2022-12-12 20:03:09 +0500

    Change form to have 2 fields, use Bootstrap components

--- src/components/Checkout/CheckoutForm/CheckoutForm.jsx
+++ src/components/Checkout/CheckoutForm/CheckoutForm.jsx
@@ -1,89 +1,59 @@
 import React, { useState } from 'react';
+import { Col, Form, Row } from 'react-bootstrap';
 
 export const CheckoutForm = () => {
-  const [form, setForm] = useState({
-    house: '',
-    street: '',
-    block: '',
-    area: '',
+  const [address, setAddress] = useState({
+    line1: '',
+    line2: '',
   });
 
   const handleFormValueChange = event => {
-    setForm(f => ({ ...f, [event.target.name]: event.target.value }));
+    setAddress(f => ({ ...f, [event.target.name]: event.target.value.trim() }));
   };
 
   const handleSubmit = event => {
     event.preventDefault();
+
+    if (!address.line1.length) return;
+
     alert(
-      `Order confirmed! Your order will be shipped to ${form.house}, Street ${form.street}, Block ${form.block}, ${form.area}!`
+      `Order confirmed! Your order will be shipped to ${address.line1}${
+        address.line2.length ? `, ${address.line2}` : ''
+      }!`
     );
   };
 
   return (
-    <form className="mt-4" onSubmit={handleSubmit}>
-      <div className="form-group">
-        <label className="text-light" htmlFor="house">
-          House
-        </label>
-        <input
-          className="form-control col-sm-12 col-md-4"
-          type="text"
-          id="house"
-          name="house"
-          value={form.house}
-          onChange={handleFormValueChange}
-          required
-        ></input>
-      </div>
+    <Form className="d-flex flex-column gap-4 mt-4" onSubmit={handleSubmit}>
+      <Row>
+        <Col md={4} className="d-flex flex-column gap-4">
+          <Form.Group controlId="line1">
+            <Form.Label className="text-white">Address Line 1</Form.Label>
+            <Form.Control
+              type="string"
+              name="line1"
+              value={address.line1}
+              onChange={handleFormValueChange}
+            />
+          </Form.Group>
 
-      <div className="form-group">
-        <label className="text-light" htmlFor="street">
-          Street
-        </label>
-        <input
-          className="form-control col-sm-12 col-md-4"
-          type="text"
-          id="street"
-          name="street"
-          value={form.street}
-          onChange={handleFormValueChange}
-          required
-        ></input>
-      </div>
+          <Form.Group controlId="line2">
+            <Form.Label className="text-white">Address Line 2</Form.Label>
+            <Form.Control
+              type="string"
+              name="line2"
+              value={address.line2}
+              onChange={handleFormValueChange}
+            />
+          </Form.Group>
+        </Col>
+      </Row>
 
-      <div className="form-group">
-        <label className="text-light" htmlFor="block">
-          Block/Sector
-        </label>
-        <input
-          className="form-control col-sm-12 col-md-4"
-          type="text"
-          id="block"
-          name="block"
-          value={form.block}
-          onChange={handleFormValueChange}
-          required
-        ></input>
-      </div>
-
-      <div className="form-group">
-        <label className="text-light" htmlFor="area">
-          Area
-        </label>
-        <input
-          className="form-control col-sm-12 col-md-4"
-          type="text"
-          name="area"
-          id="area"
-          value={form.area}
-          onChange={handleFormValueChange}
-          required
-        ></input>
-      </div>
-
-      <button className="btn btn-primary mt-3" type="submit">
-        Submit
-      </button>
-    </form>
+      <Row>
+        <Col md={2}>
+          <button className="btn btn-primary mt-3">Checkout</button>
+        </Col>
+      </Row>
+    </Form>
   );
 };
