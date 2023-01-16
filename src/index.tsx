08ae7e344a369860ed02dc0db692abdb1a76ec6e
Ali Nauman <alinauman97@outlook.com>
2023-01-16 23:51:03 +0500

    Add type assertion for container

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,6 @@ import { App } from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(container as HTMLDivElement);
 
 root.render(<App />);
