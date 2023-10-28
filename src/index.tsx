fc05ef284be877fbf14dffff3b45b1fb62d8fb8a
Ali Nauman Qureshi <alinauman97@outlook.com>
2023-10-28 18:38:26 +0500

    Wrap App in StrictMode

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const container = document.getElementById('root');
 if (!container) throw new Error('Cannot find element with selector #root');
 
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
